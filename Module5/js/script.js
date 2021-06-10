fc5b39beecad13af750cdfa92058e8811539fddc
domowhite <84952522+domowhite@users.noreply.github.com>
2021-06-09 21:39:26 -0600

    Update script.js

--- Module5/js/script.js
+++ Module5/js/script.js
@@ -104,7 +104,7 @@ function buildAndShowHomeHTML (categories) {
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
-      var chosenCategoryShortName = dc.chooseRandomCategory
+      var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
 
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
@@ -119,7 +119,7 @@ function buildAndShowHomeHTML (categories) {
       // it into the home html snippet.
       //
        var homeHtmlToInsertIntoMainPage = function (homeHtmlUrl) {
-         insertProperty (homeHtmlUrl,"randomCategoryShortName",chosenCategoryShortName); };
+         insertProperty(homeHtml,"randomCategoryShortName",chosenCategoryShortName); };
 
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
